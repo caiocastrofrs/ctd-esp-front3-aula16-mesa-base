f7e23d7c47219589b11f5e9b5dceb947adbc4edb
Caio <caiocastrofrs@gmail.com>
2022-11-30 20:30:46 -0300

    exercicio finalizado

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,72 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const App = () => {
+  const { register, handleSubmit, formState } = useForm();
+
+  const { errors } = formState;
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <main>
       <h1>Cadastro blog de receitas</h1>
 
-      <form>
-        <input type="text" placeholder="Digite seu nome" />
+      <form onSubmit={handleSubmit(onSubmit)}>
         <p>Nome é obrigatório</p>
+        <input
+          type="text"
+          placeholder="Digite seu nome"
+          {...register("name")}
+          required
+        />
+        {errors.name?.type === "required" && "Nome é obrigatório"}
 
-        <input type="email" placeholder="Digite seu email" />
         <p>Digite um email</p>
+        <input
+          type="email"
+          placeholder="Digite seu email"
+          {...register("email")}
+        />
 
-        <select>
+        <p>Selecione o tipo de usuário</p>
+        <select {...register("user_type")}>
           <option value="">Selecionar...</option>
           <option value="Leitor">Leitor</option>
           <option value="Criador">Criador de artigos</option>
         </select>
-        <p>Selecione o tipo de usuário</p>
 
+        <h3>Gênero</h3>
         <div className="gender">
-          <h3>Gênero</h3>
           <div>
-            <input type="radio" value="Male" />
+            <input type="radio" value="Male" {...register("gender")} />
             <label>Masculino</label>
           </div>
           <div>
-            <input type="radio" value="Female" />
+            <input type="radio" value="Female" {...register("gender")} />
             <label>Feminino</label>
           </div>
         </div>
 
-        <input type="number" placeholder="Digite sua idade" />
         <p>Idade é obrigatória</p>
+        <input
+          type="number"
+          placeholder="Digite sua idade"
+          {...register("age")}
+        />
 
-        <input type="password" placeholder="Digite sua senha" />
         <p>Senha é obrigatória</p>
-
-        <div className="checkbox">
-          <input type="checkbox" />
-          <label>Concordar com os termos</label>
-        </div>
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          {...register("password")}
+        />
 
         <p>Você deve concordar com os termos</p>
+        <div className="checkbox">
+          <input type="checkbox" {...register("agreedWithTerms")} />
+          <label>Concordar com os termos</label>
+        </div>
 
         <input type="submit" value="Cadastrar" />
       </form>
